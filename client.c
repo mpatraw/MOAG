b23a70b62737d0a9ebfd03df77e4100bbd9c7b08
Michael Patraw <xjabberwocky@gmail.com>
2012-05-13 14:09:31 -0700

    Color!

--- client.c
+++ client.c
@@ -59,14 +59,14 @@ bool kfire = false;
 
 void draw_tank(int x, int y, int turretangle, bool facingleft)
 {
-    draw_sprite(x, y, COLOR(240, 240, 240), tanksprite, TANK_WIDTH, TANK_HEIGHT);
+    draw_sprite(x, y, COLOR_WHITE, tanksprite, TANK_WIDTH, TANK_HEIGHT);
 
     /* 9 is the length of the cannon. */
     int ex = 9 * cos(DEG2RAD(turretangle)) * (facingleft ? -1 : 1);
     int ey = 9 * sin(DEG2RAD(turretangle)) * -1;
     draw_line(x + TANK_WIDTH / 2, y + TANK_HEIGHT / 2,
               x + TANK_WIDTH / 2 + ex, y + TANK_HEIGHT / 2 + ey,
-              COLOR(240, 240, 240));
+              COLOR_WHITE);
 }
 
 void draw_crate(int x, int y)
@@ -86,63 +86,75 @@ void draw_bullets(struct moag *m)
 
 void del_chat_line(void)
 {
-    if(chatlines[0].str && chatlines[0].expire<SDL_GetTicks()){
+    if (chatlines[0].str && chatlines[0].expire < SDL_GetTicks())
+    {
         free(chatlines[0].str);
-        for(int i=0;i<CHAT_LINES-1;i++){
-            chatlines[i].expire=chatlines[i+1].expire;
-            chatlines[i].str=chatlines[i+1].str;
+        for (int i = 0; i < CHAT_LINES - 1; i++){
+            chatlines[i].expire = chatlines[i + 1].expire;
+            chatlines[i].str = chatlines[i + 1].str;
         }
-        chatlines[CHAT_LINES-1].str=NULL;
+        chatlines[CHAT_LINES - 1].str = NULL;
     }
 }
 
 void add_chat_line(char* str)
 {
-    int i=0;
-    while(chatlines[i].str)
-        if(++i>=CHAT_LINES){
-            chatlines[0].expire=0;
+    int i = 0;
+    while (chatlines[i].str)
+    {
+        if (++i >= CHAT_LINES)
+        {
+            chatlines[0].expire = 0;
             del_chat_line();
-            i=CHAT_LINES-1;
+            i = CHAT_LINES - 1;
             break;
         }
-    chatlines[i].str=str;
-    chatlines[i].expire=SDL_GetTicks()+CHAT_EXPIRETIME;
+    }
+    chatlines[i].str = str;
+    chatlines[i].expire = SDL_GetTicks() + CHAT_EXPIRETIME;
 }
 
 void draw(struct moag *m)
 {
-    for (int x = 0; x < LAND_WIDTH; ++x) {
-        for (int y = 0; y < LAND_HEIGHT; ++y) {
+    for (int x = 0; x < LAND_WIDTH; ++x)
+    {
+        for (int y = 0; y < LAND_HEIGHT; ++y)
+        {
             if (get_land_at(m, x, y))
-                set_pixel(x, y, COLOR(155, 155, 155));
+                set_pixel(x, y, COLOR_GRAY);
         }
     }
-    for(int i=0;i<MAX_PLAYERS;i++)
-        if(m->tanks[i].active) {
+    for (int i = 0; i < MAX_PLAYERS; i++)
+    {
+        if (m->tanks[i].active)
+        {
             draw_tank(m->tanks[i].x - 9,
                       m->tanks[i].y - 13,
                       m->tanks[i].angle,
                       m->tanks[i].facingleft);
             draw_string_centered(m->tanks[i].x,
                                  m->tanks[i].y - 36,
-                                 COLOR(240, 240, 240),
+                                 COLOR_WHITE,
                                  m->tanks[i].name);
         }
-    if(m->crate.active)
+    }
+    if (m->crate.active)
         draw_crate(m->crate.x-4,m->crate.y-8);
     draw_bullets(m);
     del_chat_line();
-    for(int i=0;i<CHAT_LINES;i++)
-        if(chatlines[i].str)
+    for (int i = 0; i < CHAT_LINES; i++)
+    {
+        if (chatlines[i].str)
         {
-            draw_string(4,4+12*i, COLOR_WHITE, chatlines[i].str);
+            draw_string(4, 4 + 12 * i, COLOR_WHITE, chatlines[i].str);
             int w, h;
             get_string_size(chatlines[i].str, &w, &h);
         }
-    if (typing_str){
-        draw_block(6,8+12*(CHAT_LINES),4,4,COLOR(210,210,210));
-        draw_string(16,4+12*(CHAT_LINES),COLOR(210,210,210),typing_str);
+    }
+    if (typing_str)
+    {
+        draw_block(6, 8 + 12 * (CHAT_LINES), 4, 4, COLOR_DARK_GRAY);
+        draw_string(16, 4 + 12 * (CHAT_LINES), COLOR_DARK_GRAY, typing_str);
     }
 }
 
@@ -313,16 +325,19 @@ int main(int argc, char *argv[])
 
         grab_events();
 
-        if (typing_str && is_text_input()) {
+        if (typing_str && is_text_input())
+        {
             if (is_key_down(SDLK_ESCAPE)
                 || is_key_down(SDLK_LEFT)
                 || is_key_down(SDLK_RIGHT)
                 || is_key_down(SDLK_UP)
-                || is_key_down(SDLK_DOWN)) {
-                typing_str=NULL;
+                || is_key_down(SDLK_DOWN))
+            {
+                typing_str = NULL;
                 stop_text_input();
             }
-            else if(is_key_down(SDLK_RETURN)) {
+            else if (is_key_down(SDLK_RETURN))
+            {
                 unsigned char buffer[256];
                 size_t pos = 0;
 
@@ -339,77 +354,92 @@ int main(int argc, char *argv[])
                 stop_text_input();
             }
         }
-        else {
-            if (is_key_down(SDLK_LEFT) && !kleft) {
+        else
+        {
+            if (is_key_down(SDLK_LEFT) && !kleft)
+            {
                 send_byte(KLEFT_PRESSED_CHUNK);
                 kleft = true;
             }
-            else if (!is_key_down(SDLK_LEFT) && kleft) {
+            else if (!is_key_down(SDLK_LEFT) && kleft)
+            {
                 send_byte(KLEFT_RELEASED_CHUNK);
                 kleft = false;
             }
 
-            if (is_key_down(SDLK_RIGHT) && !kright) {
+            if (is_key_down(SDLK_RIGHT) && !kright)
+            {
                 send_byte(KRIGHT_PRESSED_CHUNK);
                 kright = true;
             }
-            else if (!is_key_down(SDLK_RIGHT) && kright) {
+            else if (!is_key_down(SDLK_RIGHT) && kright)
+            {
                 send_byte(KRIGHT_RELEASED_CHUNK);
                 kright = false;
             }
 
-            if (is_key_down(SDLK_UP) && !kup) {
+            if (is_key_down(SDLK_UP) && !kup)
+            {
                 send_byte(KUP_PRESSED_CHUNK);
                 kup = true;
             }
-            else if (!is_key_down(SDLK_UP) && kup) {
+            else if (!is_key_down(SDLK_UP) && kup)
+            {
                 send_byte(KUP_RELEASED_CHUNK);
                 kup = false;
             }
 
-            if (is_key_down(SDLK_DOWN) && !kdown) {
+            if (is_key_down(SDLK_DOWN) && !kdown)
+            {
                 send_byte(KDOWN_PRESSED_CHUNK);
                 kdown = true;
             }
-            else if (!is_key_down(SDLK_DOWN) && kdown) {
+            else if (!is_key_down(SDLK_DOWN) && kdown)
+            {
                 send_byte(KDOWN_RELEASED_CHUNK);
                 kdown = false;
             }
 
-            if (is_key_down(' ') && !kfire) {
+            if (is_key_down(' ') && !kfire)
+            {
                 send_byte(KFIRE_PRESSED_CHUNK);
                 kfire = true;
             }
-            else if (!is_key_down(' ') && kfire) {
+            else if (!is_key_down(' ') && kfire)
+            {
                 send_byte(KFIRE_RELEASED_CHUNK);
                 kfire = false;
             }
 
-            if(is_key_down('t')){
-                typing_str=start_text_input();
+            if (is_key_down('t'))
+            {
+                typing_str = start_text_input();
             }
-            if(is_key_down('/')){
-                typing_str=start_text_cmd_input();
+            if (is_key_down('/'))
+            {
+                typing_str = start_text_cmd_input();
             }
         }
 
-        while (enet_host_service(get_client_host(), &enet_ev, 10)) {
-            switch (enet_ev.type) {
-            case ENET_EVENT_TYPE_CONNECT:
-                break;
+        while (enet_host_service(get_client_host(), &enet_ev, 10))
+        {
+            switch (enet_ev.type)
+            {
+                case ENET_EVENT_TYPE_CONNECT:
+                    break;
 
-            case ENET_EVENT_TYPE_DISCONNECT:
-                INFO("Disconnected from server.\n");
-                close_window();
-                break;
+                case ENET_EVENT_TYPE_DISCONNECT:
+                    INFO("Disconnected from server.\n");
+                    close_window();
+                    break;
 
-            case ENET_EVENT_TYPE_RECEIVE:
-                on_receive(&moag, &enet_ev);
-                enet_packet_destroy(enet_ev.packet);
-                break;
+                case ENET_EVENT_TYPE_RECEIVE:
+                    on_receive(&moag, &enet_ev);
+                    enet_packet_destroy(enet_ev.packet);
+                    break;
 
-            default:
-                break;
+                default:
+                    break;
             }
         }
 
